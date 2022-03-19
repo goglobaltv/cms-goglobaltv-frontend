0a4ca80669468195338949f14bcbc74083772a05
goglobalschooldev <goglobalschool15@gmail.com>
2022-03-19 17:26:29 +0700

    ffff

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from "../Function/fn"
 
 export default axios.create({
-    baseURL: 'https://cms_endpoint.go-globaltv.com',
+    baseURL: process.env.REACT_APP_PUBLIC_DATABASE,
     // baseURL: 'http://192.168.2.183:6600',
     headers: {
         'Access-Control-Allow-Origin': '*',
